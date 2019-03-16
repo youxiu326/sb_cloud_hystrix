5ace8f5a70f0a0a49d5b352a512c3949539dfd1e
youxiu326 <youxiu326@163.com>
2019-03-16 15:09:23 +0800

    超时时间设置

--- src/main/java/com/huarui/SbCloudHystrixApplication.java
+++ src/main/java/com/huarui/SbCloudHystrixApplication.java
@@ -2,8 +2,20 @@ package com.huarui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+
+//@EnableFeignClients(basePackages = "")
+
+/*
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+*/
+@SpringCloudApplication
 public class SbCloudHystrixApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/huarui/controller/HystrixController.java
+++ src/main/java/com/huarui/controller/HystrixController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.Arrays;
 
 /**
@@ -25,18 +24,16 @@ public class HystrixController {
     public String getProductList(){
 
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.postForObject("http://39.108.85.204:8005/",
-                Arrays.asList("123"),
+        return restTemplate.getForObject("http://localhost:9000/product/getMsg",
                 String.class);
     }
 
-    @HystrixCommand(commandKey = "getProductListDefa")
+    @HystrixCommand(commandKey = "getProductListDefa")  //commandKey 默认就是方法名，用来在配置文件中单独设置超时时间
     @GetMapping("/getProductList2")
     public String getProductListDefa(){
 
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.postForObject("http://39.108.85.204:8005/",
-                Arrays.asList("123"),
+        return restTemplate.getForObject("http://localhost:9000/product/getMsg",
                 String.class);
     }
 
